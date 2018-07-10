e1a9f70ec8130f3d2707255a70177ce251c3c9f9
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2018-07-10 00:00:18 -0500

    Update Graph.c

--- Graph.c
+++ Graph.c
@@ -1,29 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafo.h"
+
+int contador=0;
+
 struct strNode{
 	Type Data;
 	struct strNode *first, *next;
 	int id;
 };
 
-typedef struct strNode Node;
- 
+typedef struct strNode *Node;
+
+Node newNode(Type data, int* contador){
+	Node new=(Node)malloc(sizeof(Node));
+	if(new!=NULL){
+		new->first=NULL;
+		new->next=NULL;
+		new->id=*contador;
+		new->Data=data;
+	}
+	(*contador)++;
+	return new;
+}
+
 struct strGraph{ //No sé si esté bien esta estructura
-	Node *array=(Node*)malloc(sizeof(Node*));  //Como tiene que ser un arreglo de vertices mejor usar Node
+	Node *array;
+	//Como tiene que ser un arreglo de vertices mejor usar Node
 	int vertices, aristas;
 	CMP cmpFunction;
 	Print printFunction;
 };
 
-
-Node* newNode(Type data){
-	Node *new=(Node*)malloc(sizeof(Node));
-	if(new!=NULL){
-		new->first=NULL;
-		new->next=NULL;
-		new->id=0;
-		new->Data=data;
-	}
-	return new;
-}
 
 Graph graph_create(CMP comparar, Print printFunction){
 	Graph newGraph=(Graph)malloc(sizeof(struct strGraph));
@@ -32,26 +40,31 @@ Graph graph_create(CMP comparar, Print printFunction){
 		newGraph->vertices=0;
 		newGraph->cmpFunction=comparar;
 		newGraph->printFunction=printFunction;
+		newGraph->array = (Node)malloc(sizeof(Node));
 	}
 	return newGraph;
 }
 
 void graph_destroy(Graph g){
+	int i;
+	while(g->array[i]->next!=NULL){
+		free(g->array[i]);
+		i++;
+	}
 	free(g);
 }
 
 boolean graph_addVertex(Graph g, Type data){ //No supe hacer esta
 	if(g!=NULL){
-		Node newNode;
+		Node nuevo;
+		nuevo=newNode(data, &contador);
 		g->array[g->vertices]=data;
 		g->vertices++;
-		newNode->first=g;
-		newNode->next=NULL;
 	}
 }
 
 boolean graph_addEdge(Graph g, Type source, Type skin){
-	
+
 }
 
 unsigned long graph_vertexCount(Graph g){
@@ -69,13 +82,13 @@ unsigned long graph_edgeCount(Graph g){
 }
 
 unsigned long graph_outDegree(Graph g, Type source){
-	
+
 }
 
 boolean graph_hasEdge(Graph g, Type source, Type skin){
-	
+
 }
 
 boolean graph_print(Graph g, Print p){
-	
+
 }
