b49f457c02fdffecaa94765f6ae0e5d95cebfac5
ie714715 <39529828+sofmich@users.noreply.github.com>
2018-07-10 20:24:15 -0500

    List - v1

---
+++ List.c
@@ -0,0 +1,127 @@
+/*
+ * List.c
+ *
+ *  Created on: 10/07/2018
+ *      Author: Momo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+struct strNode{
+	Type data;
+	struct strNode *next;
+	struct strNode *prior;
+};
+typedef struct strNode* Node;
+struct strList{
+	Node First;
+	Node Last;
+	int size;
+};
+List list_create(){
+	List new=(List)malloc(sizeof(struct strList));
+	if(new!=NULL){
+		new->First=NULL;
+		new->Last=NULL;
+		new->size=0;
+	}
+	return new;
+}
+int list_size(List who){
+	return who->size;
+}
+Bool list_add(List who, Type data){
+	Node newNode=(Node)malloc(sizeof(struct strNode));
+	if (newNode==NULL)
+		return false;
+	newNode->data=data;
+	newNode->next=NULL;
+	if(list_size(who)==0){
+		who->First=newNode;
+		newNode->prior=NULL;
+	}
+	else{
+		newNode->prior=who->Last;
+		who->Last->next=newNode;
+	}
+	who->Last=newNode;
+	who->size++;
+	return true;
+}
+Bool list_insert(List who, int p, Type data){
+	if(p>list_size(who)+1||p<1)
+		return false;
+	Node newNode=(Node)malloc(sizeof(struct strNode));
+	if (newNode==NULL)
+		return false;
+	newNode->data=data;
+	if(p==1){
+		newNode->prior=NULL;
+		newNode->next=who->First;
+		if(who->First==NULL)
+			who->Last=newNode;
+		else
+			who->First->prior=newNode;
+		who->First=newNode;
+	}
+	else{
+		Node tempNode=who->First;
+		for(int i=1;i<p-1;i++)
+			tempNode=tempNode->next;
+		newNode->prior=tempNode;
+		newNode->next=tempNode->next;
+		tempNode->next=newNode;
+		if(newNode->next!=NULL)
+			newNode->next->prior=newNode;
+		else
+			who->Last=newNode;
+	}
+	who->size++;
+	return true;
+}
+
+Type list_remove(List who, int p){
+	if(p>list_size(who)||p<1)
+		return NULL;
+	Type data;
+	Node tempNode=who->First;
+	for(int i=1;i<p;i++)
+		tempNode=tempNode->next;
+	data=tempNode->data;
+	if(tempNode->prior!=NULL)
+		tempNode->prior->next=tempNode->next;
+	else
+		who->First=tempNode->next;
+	if(tempNode->next!=NULL)
+		tempNode->next->prior=tempNode->prior;
+	else
+		who->Last=tempNode->prior;
+	who->size--;
+	free(tempNode);
+	return data;
+}
+Type list_get(List who, int p){
+	if(p>list_size(who)||p<1)
+		return NULL;
+	Node tempNode=who->First;
+	for(int i=1;i<p;i++)
+		tempNode=tempNode->next;
+	return tempNode->data;
+}
+void list_set(List who, Type data, int p){
+	if(p<=list_size(who)&&p>0){
+		Node tempNode=who->First;
+		for(int i=1;i<p;i++)
+			tempNode=tempNode->next;
+		tempNode->data=data;
+	}
+}
+void list_destroy(List who){
+	Type temp;
+	while(list_size(who)!=0)
+		temp=list_remove(who,1);
+	free(who);
+}
+
---
+++ List.h
@@ -0,0 +1,24 @@
+/*
+ * List.h
+ *
+ *  Created on: 10/07/2018
+ *      Author: Momo
+ */
+
+#ifndef LIST_H_
+#define LIST_H_
+typedef void* Type;
+typedef struct strList* List;
+typedef enum{false,true}Bool;
+
+List list_create();
+int list_size(List who);
+Bool list_add(List who, Type data);
+Type list_get(List who, int p);
+void list_set(List who, Type, int p);
+Type list_remove(List who, int p);
+Bool list_insert(List who, int p, Type value);
+void list_destroy(List who);
+
+
+#endif /* LIST_H_ */
