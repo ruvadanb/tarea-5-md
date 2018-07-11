ea32e146b54b31dcc932a7122c37576896dcef20
ie714715 <39529828+sofmich@users.noreply.github.com>
2018-07-11 06:23:24 -0500

    Add files via upload

--- Tarea5_1.c
+++ Tarea5_1.c
@@ -13,11 +13,46 @@
 #include <string.h>
 #include "Graph.h"
 
-int main(){
+int compareValues(Type, Type);
+void printValue(Type);
+unsigned indexValue(Type, unsigned);
 
+typedef struct{
+	char Name[25];
+	char Date[4];
+	char Age[10];
+}Person;
+typedef struct{
+	char Tittle[25];
+	char Date[4];
+	char Tagline[40];
+}Movies;
+typedef struct{
+	char Name[25];
+	char Tittle[25];
+}Edge;
+int main(){
+	FILE *v_person = fopen("vertex_person.csv","r");
+	FILE *v_movies = fopen("vertex_movies.csv","r");
+	FILE *edges = fopen("edges_PersonMovies.csv","r");
+	if(!v_person || !v_movies || !edges) {
+		printf("Failed opening files...\n");
+		return 0;
+	}else{
+		Graph g_Person = graph_create(compareValues, printValue, indexValue);
+		Graph g_Movies = graph_create(compareValues, printValue, indexValue);
+	}
 	return 0;
 }
+int compareValues(Type data1, Type data2){
+	return 0;
+}
+void printValue(Type data){
 
+}
+unsigned indexValue(Type data, unsigned size){
+	return 0;
+}
 
 
 
